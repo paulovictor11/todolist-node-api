0296b0e31ce8b8f4043fbbee7c1bdb5a6a8d6875
paulovictor11 <pauloalmeidajp652@gmail.com>
2022-06-02 20:22:32 -0300

    create tests for delete user usecase

---
+++ src/tests/usecases/user/delete-usecase.spec.ts
@@ -0,0 +1,50 @@
+import { faker } from "@faker-js/faker";
+import { NotFoundError } from "../../../app/presentation/errors/not-found-error";
+import { DeleteUserUsecase } from "../../../app/usecases/user/delete.usecase";
+import { MissingParamError } from "../../../app/utils/errors/missing-param-error";
+import { User } from "../../../domain/user";
+import { InMemoryUserRepository } from "../../repositories/inMemoryUserRepository";
+
+const makeSut = () => {
+    const repository = new InMemoryUserRepository();
+    const sut = new DeleteUserUsecase(repository);
+
+    return {
+        repository,
+        sut,
+    };
+};
+
+const userSpy = {
+    id: faker.datatype.number(),
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+};
+
+describe("Delete user usecase", () => {
+    it("should throw an error when no user id is provided", async () => {
+        const { sut } = makeSut();
+        const promise = sut.execute(Number(""));
+
+        expect(promise).rejects.toThrow(new MissingParamError("user id"));
+    });
+
+    it("should throw an error when no user is founded", async () => {
+        const { sut } = makeSut();
+        const promise = sut.execute(userSpy.id);
+
+        expect(promise).rejects.toThrow(new NotFoundError("user"));
+    });
+
+    it("should be able to delete an user", async () => {
+        const { sut, repository } = makeSut();
+        const user = new User(userSpy);
+        await repository.create(user);
+
+        const promise = sut.execute(userSpy.id);
+
+        expect(promise).resolves.not.toThrow();
+        expect(await promise).toBeUndefined();
+    });
+});
