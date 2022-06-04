81f22898bf924ec4ddc679d65c3def12081039db
paulovictor11 <pauloalmeidajp652@gmail.com>
2022-06-04 14:12:28 -0300

    create tests for find project by id usecase

---
+++ src/tests/usecases/project/find-by-id.spec.ts
@@ -0,0 +1,51 @@
+import { faker } from "@faker-js/faker";
+import { NotFoundError } from "../../../app/presentation/errors/not-found-error";
+import { FindProjectByIdUsecase } from "../../../app/usecases/project/find-by-id.usecase";
+import { MissingParamError } from "../../../app/utils/errors/missing-param-error";
+import { Project } from "../../../domain/project";
+import { InMemoryProjectRepository } from "../../repositories/inMemoryProjectRepository";
+
+const makeSut = () => {
+    const repository = new InMemoryProjectRepository();
+    const sut = new FindProjectByIdUsecase(repository);
+
+    return {
+        repository,
+        sut,
+    };
+};
+
+const projectSpy = {
+    id: faker.datatype.number(),
+    title: faker.lorem.sentence(),
+    description: faker.lorem.paragraph(),
+    userId: faker.datatype.number(),
+    task: [],
+};
+
+describe("Find project by id", () => {
+    it("should throw an error when no project id is provided", async () => {
+        const { sut } = makeSut();
+        const promise = sut.execute(Number(""));
+
+        expect(promise).rejects.toThrow(new MissingParamError("project id"));
+    });
+
+    it("should throw an error when no project is founded", async () => {
+        const { sut } = makeSut();
+        const promise = sut.execute(projectSpy.id);
+
+        expect(promise).rejects.toThrow(new NotFoundError("project"));
+    });
+
+    it("should be able to find a project", async () => {
+        const { sut, repository } = makeSut();
+        const project = new Project(projectSpy);
+        await repository.create(project);
+
+        const promise = sut.execute(projectSpy.id);
+
+        expect(promise).resolves.not.toThrow();
+        expect(await promise).toStrictEqual(project.toJson(true));
+    });
+});
